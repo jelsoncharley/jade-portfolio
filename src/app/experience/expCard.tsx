085c4e1d6980b1bfeb3ac96b35300ba0a763ddbb
jelsoncharley <jelsoncharley@yahoo.co.in>
2025-08-27 21:05:04 -0300

    WIP: functions server client props

--- src/app/experience/expCard.tsx
+++ src/app/experience/expCard.tsx
@@ -1,13 +1,20 @@
+'use client'
 import React from "react"
 
 type ExperienceProps = {
     image: string,
     name: string,
     position: string,
-    duration: string
+    duration: string,
+    // test: () => void
 }
 
-export const ExpCard: React.FC<ExperienceProps> = ({ image, name, position, duration }) => {
+export const ExpCard: React.FC<ExperienceProps> = ({ image, name, position, duration/*, test*/ }) => {
+
+    const check = (event: any) => {
+        console.log(event)
+    }
+
     return (<>
         <div className="w-full max-w-xs overflow-hidden rounded-lg border border-slate-200 bg-white shadow-lg shadow-slate-950/5">
             {/* <img className="m-1.5 h-max w-[calc(100%-12px)]" src={image} alt="image" /> */}
@@ -24,7 +31,7 @@ export const ExpCard: React.FC<ExperienceProps> = ({ image, name, position, dura
                 </p>
             </div>
             <div className="w-full rounded px-3 pb-3 pt-1.5">
-                <button className="inline-flex rounded-md border border-slate-800 bg-slate-800 px-4 py-2 text-center font-sans text-sm font-medium text-slate-50 transition-all duration-300 ease-in hover:border-slate-700 hover:bg-slate-700 disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none">
+                <button onClick={(e) => { check(e) }} className="inline-flex rounded-md border border-slate-800 bg-slate-800 px-4 py-2 text-center font-sans text-sm font-medium text-slate-50 transition-all duration-300 ease-in hover:border-slate-700 hover:bg-slate-700 disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none">
                     Know More
                 </button>
             </div>
--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,19 +1,29 @@
+
+'use server'
 import { ExpCard } from "./expCard";
 import * as data from "../../data/company.json"
 
-export default function Experience() {
+export default async function Experience() {
+
+
+    const handleCheck = async (key: any) => {
+        'use server'
+        console.log(key)
+    }
+
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
             <p>
                 Experience
             </p>
             <div className="lg:flex gap-5">
+                {/* <ExpCard key={data.company[0].name} image={data.company[0].image} name={data.company[0].name} position={data.company[0].position} duration={data.company[0].duration} ></ExpCard> */}
                 {
-                    data.company.map((company) => {
-                        return <ExpCard image={company.image} name={company.name} position={company.position} duration={company.duration}></ExpCard>
+                    data.company.map((company, idx) => {
+                        return <ExpCard key={company.name + idx} image={company.image} name={company.name} position={company.position} duration={company.duration}></ExpCard>
                     })
                 }
             </div>
-        </div>
+        </div >
     );
 }
